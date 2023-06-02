8521365ea3daf9d4ca7bcc8fe679dd3376b8c95f
SadmanIshraqMohiuddin <38718132+SadmanIshraqMohiuddin@users.noreply.github.com>
2023-06-02 21:07:50 +0600

    Add files via upload
    Tutoring online script - (make sure to hardcode it with the class zoom link, student emails, pre-composed email)
    - Run script 
    - Script checks the day and automatically creates email to send to students for that day [You just have to press the final send button]
    - Script opens timesheet on google docs
    - Script turns on zoom link for you

---
+++ Tutoring_zoom.py
@@ -0,0 +1,62 @@
+import datetime
+import webbrowser
+import urllib.parse
+
+# Get current day
+current_day = datetime.datetime.now().strftime("%A")
+
+# Dictionary mapped: days to email recipients
+email_recipients = {
+    "Monday": ["student_email@domain.com"],
+    "Tuesday": ["student_email@domain.com"],
+    "Wednesday": ["student_email@domain.com"],
+    "Thursday": ["student_email@domain.com"],
+    "Friday": ["student_email@domain.com"],
+    "Saturday": ["student_email@domain.com"],
+    "Sunday": ["student_email@domain.com"]
+}
+
+# Check if current day in dict
+if current_day in email_recipients:
+    recipient_emails = email_recipients[current_day]
+
+    # Compose Email with details
+    subject = "Link"
+    body = """Hello,
+
+Here is your class link.
+
+======================
+
+Sadman Mohiuddin is inviting you to a scheduled Zoom meeting.
+
+Join Zoom Meeting:
+
+{Type in your zoom link here}
+
+Meeting ID: Type in your zoom meeting ID here
+Passcode: Type in your zoom Passcode here
+
+======================
+
+Best Regards,
+Sadman Mohiuddin
+"""
+    body_encoded = urllib.parse.quote(body)
+    recipient_emails_encoded = ','.join([urllib.parse.quote(email) for email in recipient_emails])
+    compose_url = f"https://mail.google.com/mail/?view=cm&fs=1&to={recipient_emails_encoded}&su={urllib.parse.quote(subject)}&body={body_encoded}"
+
+    # Open time sheet
+    time_sheet = "Type in your google sheet time sheet link here"
+    webbrowser.open(time_sheet)
+    
+    # Open the compose URL 
+    webbrowser.open(compose_url)
+
+    # Open  Zoom link 
+    zoom_link = "Type in your zoom link here"
+    webbrowser.open(zoom_link)
+
+    print("Entered emails:", recipient_emails)
+else:
+    print("No email recipients specified for", current_day)
